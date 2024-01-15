6cba2d14f158b3e8e178635368dde4adbfa8451d
Vincent1liu <liuyouming92@gmail.com>
2024-01-16 09:14:36 +1100

    event2

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,27 +1,12 @@
-const isLogin = true;
+// import { useState,useRef,useEffect } from "react"; 
 
-function App() {
-  // const handleClick = () => {console.log('button was clicked')};
+function App (){
+    return (
+        <div className="App">
+            This is App
 
-
-  // 获取事件参数 e
-  const handleClick = (e) => {console.log('e button was clicked',e)};
-
-  // 获取自定义参数
-  const handleClick1 = (e) => {console.log('e button was clicked 2',e)};
-  return (
-    <div className="App">
-      This is a React App
-
-      { /*条件渲染 */ }
-      {isLogin && <span> This is span </span>}
-      {isLogin ?  <span> isLogin is true </span> :  <span> This is loading </span>}
-
-      { /* 事件绑定 */ }
-      <button onClick = {handleClick}> click me </button>
-      <button onClick = {()=> handleClick1("Vincent")}> click me 1 </button>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
